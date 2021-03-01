8a147efed6338a3c3bc72cd43c3cc04dcb8aa384
giordanoale <a.giordano89@gmail.com>
2021-03-01 18:19:27 +0100

    Completato Email Verifier

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -2,14 +2,15 @@
 
 var email = [ "giovanni.giorgio@gmail.com", "mario.rossi@gmail.com", "a.giordano89@gmail.com", "ernesto.meda@gmail.com", "diego.armando@gmail.com"];
 
-var utente = prompt("Inserisci la tua mail");
+var utente = prompt("Benvenuto! Prima di inizare inserisci il tuo indirizzo email qui sotto");
+
+console.log("La mail inserita dall'utente è " + utente);
 
 var emailGiusta;
 
 // 2. Confrontarla con le altre email
 
 for (var i = 0; i < email.length; i++) {
-  console.log(email[i]);
   if (utente == email[i]) {
     console.log("Valido!")
     emailGiusta = 1;
@@ -20,6 +21,10 @@ for (var i = 0; i < email.length; i++) {
 
 if (emailGiusta == 1) {
   console.log("Puoi entrare");
+  document.getElementById("messaggio").innerHTML = "Accesso Garantito"
+  document.getElementById("disclaimer").innerHTML = "La tua mail è giusta e puoi proseguire con la navigazione."
 } else {
   console.log("You shall not pass!");
+  document.getElementById("messaggio").innerHTML = "You shall not pass!"
+  document.getElementById("disclaimer").innerHTML = "La mail che hai inserito è errata, ricarica la pagina e prova con un altro indirizzo."
 }

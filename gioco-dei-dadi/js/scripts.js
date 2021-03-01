a019890c7a1188bece44667dfd3125662a848c5a
giordanoale <a.giordano89@gmail.com>
2021-03-01 15:54:18 +0100

    Il programma genera due numeri casuali e li stampa nel console.log

---
+++ gioco-dei-dadi/js/scripts.js
@@ -0,0 +1,10 @@
+// 1. Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+
+var numero1 = Math.floor(Math.random() * 6) + 1;
+
+var numero2 = Math.floor(Math.random() * 6) + 1;
+
+console.log(numero1);
+console.log(numero2);
+
+// 2. Stabilire il vincitore, in base a chi fa il punteggio più alto.

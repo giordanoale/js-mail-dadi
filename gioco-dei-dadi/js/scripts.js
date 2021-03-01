8d8affa5566039a64c48f270f0b5b76045cac956
giordanoale <a.giordano89@gmail.com>
2021-03-01 16:51:07 +0100

    Completato Gioco dei Dadi

--- gioco-dei-dadi/js/scripts.js
+++ gioco-dei-dadi/js/scripts.js
@@ -5,6 +5,26 @@ var numero1 = Math.floor(Math.random() * 6) + 1;
 var numero2 = Math.floor(Math.random() * 6) + 1;
 
 console.log(numero1);
+
 console.log(numero2);
 
+document.getElementById("numero1").innerHTML = numero1;
+
+document.getElementById("numero2").innerHTML = numero2;
+
 // 2. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+if (numero1 > numero2) {
+  console.log("Hai vinto, il tuo numero è " + numero1 + " il numero del computer è " + numero2);
+  document.getElementById("risultato").innerHTML = "Hai vinto!";
+}
+
+if (numero1 < numero2) {
+  console.log("Hai perso, il tuo numero è " + numero1 + " il numero del computer è " + numero2);
+  document.getElementById("risultato").innerHTML = "Hai perso!";
+}
+
+if (numero1 == numero2) {
+  console.log(" Pareggio! Il tuo numero è " + numero1 + " il numero del computer è " + numero2);
+  document.getElementById("risultato").innerHTML = "Hai pareggiato!"
+}
